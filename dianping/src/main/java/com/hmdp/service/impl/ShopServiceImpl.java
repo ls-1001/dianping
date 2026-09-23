13c6a158eb9d3c22212b2bdaeda3b696bf29ec74
86187 <2848422809@qq.com>
2026-09-23 23:47:35 +0800

    完成根据查询商品信息和获取商品类型接口

--- dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -14,7 +24,32 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryById(Long id) {
+        //先从redis缓存中查询
+        String strShop = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        if (StrUtil.isNotBlank(strShop)){
+            return Result.ok(JSONUtil.toBean(strShop, Shop.class));
+        }
+        if (strShop != null){
+            return Result.fail("Shop not found");
+        }
+        //如果redis中不存在，从mysql中查询
+        Shop shop = getById(id);
+        if (shop != null){
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
+            stringRedisTemplate.expire(CACHE_SHOP_KEY + id, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+            return Result.ok(shop);
+        }
+        //防止缓存击穿
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+
+        return Result.fail("Shop not found");
+    }
 }
--- dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -14,7 +28,44 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result typeList() {
+        // 从Redis中查询缓存
+        Map<Object, Object> shopTypeMap = stringRedisTemplate.opsForHash().entries(CACHE_SHOP_TYPE_KEY);
+        if (!shopTypeMap.isEmpty()) {
+            // 返回缓存数据shopTypeMap
+            //封装成List<ShopType>
+            List<ShopType> typeList = shopTypeMap.values().stream()
+                    .map(shopType -> JSONUtil.toBean(shopType.toString(), ShopType.class))
+                    .collect(Collectors.toList());
+            log.info("Cache hit for shop type list {}", typeList.toString());
+            return Result.ok(typeList);
+        }
+        // mysql查询店铺类型
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+        log.info("Cache miss for shop type list, querying from database {}", typeList.toString());
+        if (typeList == null){
+            return Result.fail("店铺类型查询失败");
+        }
+        //缓存到Redis
+//        stringRedisTemplate.opsForHash().putAll(CACHE_SHOP_TYPE_KEY, typeList.stream()
+//                .collect(Collectors.toMap(ShopType::getId, shopType -> JSONUtil.toJsonStr(shopType))));
+//        // ... existing code ...
+        stringRedisTemplate.opsForHash().putAll(CACHE_SHOP_TYPE_KEY, typeList.stream()
+                .collect(Collectors.toMap(
+                        shopType -> shopType.getId().toString(),
+                        shopType -> JSONUtil.toJsonStr(shopType)
+                )));
+        //设置缓存过期时间
+        stringRedisTemplate.expire(CACHE_SHOP_TYPE_KEY, CACHE_SHOP_TYPE_TTL, TimeUnit.MINUTES);
+        log.info("Cache updated for shop type list {}", typeList.toString());
+        return Result.ok(typeList);
+    }
 }
