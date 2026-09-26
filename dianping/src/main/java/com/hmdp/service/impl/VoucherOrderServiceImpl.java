f34b265db6d2382ab9a14c4cfbe7fa9251bfe0c8
86187 <2848422809@qq.com>
2026-09-26 16:34:46 +0800

    解决同一用户并发超卖，不同用户并发超卖和一人一单问题

--- dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -7,8 +7,11 @@ import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -34,7 +37,9 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private StringRedisTemplate stringRedisTemplate;
     @Resource
     private SeckillVoucherServiceImpl seckillVoucherService;
-
+    @Lazy
+    @Resource
+    private IVoucherOrderService self;
     /**
      * 秒杀优惠券
      * @param  voucherId 优惠券id
@@ -42,38 +47,53 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
      */
     @Override
     public Result seckillVoucher(Long voucherId) {
-        //查看优惠券库存
-        String stock = stringRedisTemplate.opsForValue().get(SECKILL_STOCK_KEY + voucherId);
-        if (stock == null || stock.equals("0")) {
-            //查询数据库获取库存
-            SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
-            if (seckillVoucher == null || seckillVoucher.getStock() == 0) {
-                return Result.fail("库存不足");
-            }
-            //将库存保存到Redis并设置过期时间
-            long now = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
-            long end = seckillVoucher.getEndTime().toEpochSecond(ZoneOffset.of("+8"));
-            stringRedisTemplate.opsForValue().set(SECKILL_STOCK_KEY + voucherId, seckillVoucher.getStock().toString(), end - now, TimeUnit.SECONDS);
-        }
         //获取用户ID
         Long userId = UserHolder.getUser().getId();
         if (userId == null) {
             return Result.fail("用户未登录");
         }
-        //判断用户是否已经购买过该优惠券
-        if (this.query().eq("user_id", userId).eq("voucher_id", voucherId).count() > 0) {
-            return Result.fail("用户已经购买过该优惠券");
+        //添加用户锁  防止线程并发同一用户并发重复购买
+        synchronized (userId.toString().intern()){
+            //判断用户是否已经购买过该优惠券
+            int count = this.query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+            if ( count > 0) {
+                return Result.fail("用户已经购买过该优惠券");
+            }
+            return Result.ok(createVoucherOrder(voucherId, userId));
         }
-        //扣减优惠券库存
-        //TODO MQ异步写入数据库
-        stringRedisTemplate.opsForValue().decrement(SECKILL_STOCK_KEY + voucherId);
-        //创建订单
-        VoucherOrder voucherOrder = new VoucherOrder();
-        voucherOrder.setUserId(userId);
-        voucherOrder.setVoucherId(voucherId);
-        save(voucherOrder);
-        return Result.ok(voucherOrder.getId());
     }
 
-
+    public Result createVoucherOrder(Long voucherId, Long userId) {
+        //扣减优惠券库存   判断和扣减原子操作
+        //TODO MQ异步写入数据库
+        Long stock = stringRedisTemplate.opsForValue().decrement(SECKILL_STOCK_KEY + voucherId);
+        if (stock < 0) {
+            //查询数据库获取库存
+            SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+            if (seckillVoucher == null || seckillVoucher.getStock() <= 0) {
+                stringRedisTemplate.opsForValue().increment(SECKILL_STOCK_KEY + voucherId);
+                return Result.fail("库存不足");
+            }
+            //将库存保存到Redis并设置过期时间
+            long now = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
+            long end = seckillVoucher.getEndTime().toEpochSecond(ZoneOffset.of("+8"));
+            if (now > end) {
+                stringRedisTemplate.opsForValue().increment(SECKILL_STOCK_KEY + voucherId);
+                return Result.fail("秒杀已结束");
+            }
+            stringRedisTemplate.opsForValue().set(SECKILL_STOCK_KEY + voucherId, String.valueOf(seckillVoucher.getStock() - 1), end - now, TimeUnit.SECONDS);
+        }
+        try {
+            //创建订单
+            VoucherOrder voucherOrder = new VoucherOrder();
+            voucherOrder.setUserId(userId);
+            voucherOrder.setVoucherId(voucherId);
+            save(voucherOrder);
+            return Result.ok(voucherOrder.getId());
+        } catch (Exception e) {
+            //库存加一
+            stringRedisTemplate.opsForValue().increment(SECKILL_STOCK_KEY + voucherId);
+            throw new RuntimeException(e);
+        }
+    }
 }
